6a3dc12c57d88c70c2bfdd8b75bfd96c569c872a
danan <sbzddanan@gmail.com>
2022-05-23 23:26:28 +0800

    选择难度 && 更改advanced图片重复 && 数据库连接 && 计时 && 用户欢迎

--- memorg_game.py
+++ memorg_game.py
@@ -6,6 +6,7 @@ from statistics import mean
 import tkinter
 from venv import create
 from xmlrpc.client import boolean
+from colorama import Cursor
 import pygame
 import os
 import random
@@ -17,7 +18,7 @@ rootdir = os.path.split(os.path.abspath(__file__))[0]
 sys.path.append(os.path.join(rootdir, "utils"))
 sys.path.append(os.path.join(rootdir, "src"))
 sys.path.append(os.path.join(rootdir, "constant"))
-print("DEBUG:{}".format(os.path.join(rootdir, "constant")))
+# print("DEBUG:{}".format(os.path.join(rootdir, "constant")))
 
 
 from config import Config
@@ -27,6 +28,7 @@ from menu import create_menu
 from constant import advanced_constants
 from constant import junior_constants
 from constant import mediate_constants
+from db import db_excute
 
 
 '''记忆翻牌小游戏'''
@@ -34,13 +36,22 @@ class FlipCardByMemoryGame():
     def __init__(self):
         self.cfg = Config
         self.create_login_window()
+        # 计时器flag
+        self.is_first_click = 0
+        # cursor = db_excute()
+        # cursor.execute("select version()")
+        # data = cursor.fetchone()
+        # print(data)
 
     def create_login_window(self):
         # 主界面句柄
         self.login_window = Tk()
-        self.login_window.title('Flip Card by Memory')    
+        self.login_window.title('Flip Card by Memory')   
+        # TODO: 图标
+        # self.login_window.iconbitmap('/home/danan/item/demo/resources/cat.ico') 
         self.login_window.geometry('450x300')
         # 新建文本标签
+        # TODO: 用户名
         self.username = Label(self.login_window, text = "用户名：")
         self.username.pack()
         # 创建动字符串
@@ -51,6 +62,14 @@ class FlipCardByMemoryGame():
         # TODO:grid布局
         self.guest_login_button = Button(self.login_window, text="游客登录", command=self.run_junior)
         self.guest_login_button.pack()
+        # 居中显示
+        # TODO: login居中显示
+        self.login_window.withdraw()
+        self.login_window.update_idletasks()
+        x = (self.login_window.winfo_screenwidth() - self.login_window.winfo_reqwidth()) / 2
+        y = (self.login_window.winfo_screenheight() - self.login_window.winfo_reqheight()) / 2
+        self.login_window.geometry('+%d+%d' % (x, y))
+        self.login_window.deiconify()
     
     '''help function'''
     def help(self):
@@ -83,6 +102,7 @@ class FlipCardByMemoryGame():
         self.score_sound.set_volume(1)
         # 卡片图片路径
         self.card_dir = cfg.IMAGEPATHS['carddirs'][rank]
+        print(self.card_dir)
         self.root.title('Flip Card by Memory')
         # 游戏界面中的卡片字典
         self.game_matrix = {}
@@ -91,7 +111,8 @@ class FlipCardByMemoryGame():
         # 卡片背面
         self.cards_back_image = PhotoImage(data=cfg.IMAGEPATHS['cards_back'])
         # 所有卡片的索引
-        cards_list = list(range(image_num - 1)) + list(range(image_num - 1))
+        cards_list = list(range(image_num)) + list(range(image_num))
+        print("cardlist{}".format(cards_list))
         random.shuffle(cards_list)
         # 在界面上显示所有卡片的背面
         for r in range(line):
@@ -99,9 +120,11 @@ class FlipCardByMemoryGame():
                 position = f'{r}_{c}'
                 self.game_matrix[position] = Label(self.root, image=self.cards_back_image)
                 self.game_matrix[position].back_image = self.cards_back_image
-                self.game_matrix[position].file = str(cards_list[r * line + c])
-                # print("SHOW: r is {}, r * 4 is {}, c is {}, r * 4 + c is {}".format(r, r * 4, c, r * 4 + c))
-                print("DEBUG:{}".format(self.game_matrix[position].file))
+                # r*colmn+c生成0-cards_num个数
+                self.game_matrix[position].file = str(cards_list[r * column + c])
+                print("SHOW:{}".format(self.game_matrix[position].file))
+                # print("SHOW: r is {}, r * column is {}, c is {}, r * colume + c is {}".format(r, r * column, c, r * column + c))
+                # print("DEBUG:{}".format(self.game_matrix[position].file))
                 self.game_matrix[position].show = False
                 self.game_matrix[position].bind('<Button-1>', self.clickcallback)
                 self.game_matrix[position].grid(row=r, column=c)
@@ -109,10 +132,14 @@ class FlipCardByMemoryGame():
         self.shown_cards = []
         # 场上存在的卡片数量
         self.num_existing_cards = len(cards_list)
-        # 显示游戏剩余时间
-        self.num_seconds = 30
+        # 显示游戏时间
+        self.num_seconds = 0
         self.time = Label(self.root, text=f'Time Left: {self.num_seconds}')
-        self.time.grid(row=6, column=3, columnspan=2)
+        self.time.grid(row=line + 1, column=column - 1, columnspan=2)
+        # 显示用户
+        self.user_name = '游客'
+        self.greet = Label(self.root, text=f'欢迎你! {self.user_name}')
+        self.greet.grid(row=line + 1, column=0)
         # 居中显示
         self.root.withdraw()
         self.root.update_idletasks()
@@ -120,13 +147,12 @@ class FlipCardByMemoryGame():
         y = (self.root.winfo_screenheight() - self.root.winfo_reqheight()) / 2
         self.root.geometry('+%d+%d' % (x, y))
         self.root.deiconify()
-        # 计时
-        self.tick()
+        # 显示主界面
         self.run_game()
 
 
 
-
+    # 把几个run规范一下
     def run_junior(self):
         self.create_game_window()
 
@@ -147,8 +173,14 @@ class FlipCardByMemoryGame():
 
     '''点击回调函数'''
     def clickcallback(self, event):
+        # 第一次点击任意卡片，计时器开始
+        if self.is_first_click == 0:
+            self.tick()
+            self.is_first_click = 1
         card = event.widget
-        if card.show: return
+        # print(card)
+        if card.show: 
+            return
         # 之前没有卡片被翻开
         if len(self.shown_cards) == 0:
             self.shown_cards.append(card)
@@ -167,12 +199,12 @@ class FlipCardByMemoryGame():
                     card.blank_image = self.blank_image
                     self.shown_cards.pop(0)
                     self.score_sound.play()
-                self.num_existing_cards -= 2
                 image = ImageTk.PhotoImage(Image.open(os.path.join(self.card_dir, card.file+'.png')))
                 card.configure(image=image)
                 card.show_image = image
                 card.show = True
                 card.after(300, delaycallback)
+                self.num_existing_cards -= 2
             # --之前翻开的卡片和现在的卡片不一样
             else:
                 self.shown_cards.append(card)
@@ -223,30 +255,24 @@ class FlipCardByMemoryGame():
                 self.shown_cards[-1].show_image = image
                 self.shown_cards[-1].show = True
         # 判断游戏是否已经胜利
+        print("existing cards:{}".format(self.num_existing_cards))
         if self.num_existing_cards == 0:
             is_restart = messagebox.askyesno('Game Over', 'Congratulations, you win, do you want to play again?')
             if is_restart: self.restart()
-            else: self.login_window.destroy()
+            else: self.root.destroy()
     '''计时'''
 
 
     def tick(self):
         if self.num_existing_cards == 0: return
-        if self.num_seconds != 0:
-            self.num_seconds -= 1
-            self.time['text'] = f'Time Left: {self.num_seconds}'
-            self.time.after(1000, self.tick)
-        else:
-            is_restart = messagebox.askyesno('Game Over', 'You fail since time up, do you want to play again?')
-            if is_restart: self.restart()
-            else: self.root.destroy()
+        self.num_seconds += 1
+        self.time['text'] = f'Time Left: {self.num_seconds}'
+        self.time.after(1000, self.tick)
 
         '''重新开始游戏'''
     def restart(self):
         self.root.destroy()
-        client = FlipCardByMemoryGame()
-        # TODO:重新开始的是啥的问题
-        client.run()
+        client = FlipCardByMemoryGame().run_junior()
 
     
 a = FlipCardByMemoryGame()
